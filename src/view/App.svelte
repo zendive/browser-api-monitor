c4502d54116939eb64afa1f601189a4e2ed39c92
🕷 <3756473+zendive@users.noreply.github.com>
2024-02-26 13:16:37 +0200

    extract timers component

--- src/view/App.svelte
+++ src/view/App.svelte
@@ -10,9 +10,10 @@
   } from '@/api/communication';
   import { IS_DEV } from '@/api/const';
   import { Fps } from '@/api/time';
-  import Number from './components/Number.svelte';
   import type { TMetrics } from '@/cs-main';
+  import Number from './components/Number.svelte';
   import VideoMetrics from './components/VideoMetrics.svelte';
+  import Timers from './components/Timers.svelte';
 
   let fpsValue = 0;
   const fps = new Fps((value) => (fpsValue = value)).start();
@@ -68,34 +69,7 @@
       <div>Audios: <Number bind:value={m.audiosCount} /></div>
     {/if}
 
-    {#each m.timers as api}
-      <div>
-        {#if api.invocations}
-          <strong>{api.name}:</strong>
-          <Number bind:value={api.invocations} />
-          {#if api.name === 'setTimeout'}
-            [<Number bind:value={m.timersUsages.timeouts.length} />]
-            {#each m.timersUsages.timeouts as v}
-              <li>
-                {v[0]}, [{#each v[1] as stack, index}
-                  {#if index > 0}|{/if}<a href={stack.link}>{stack.name}</a
-                  >{/each}]
-              </li>
-            {/each}
-          {/if}
-          {#if api.name === 'setInterval'}
-            [<Number bind:value={m.timersUsages.intervals.length} />]
-            {#each m.timersUsages.intervals as v}
-              <li>
-                {v[0]}, [{#each v[1] as stack, index}
-                  {#if index > 0}|{/if}<a href={stack.link}>{stack.name}</a
-                  >{/each}]
-              </li>
-            {/each}
-          {/if}
-        {/if}
-      </div>
-    {/each}
+    <Timers bind:timers={m.timers} bind:timersUsages={m.timersUsages} />
   </main>
 {/if}
 
---
+++ src/view/components/Timers.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import type { TMetrics } from '@/cs-main';
+  import Number from './Number.svelte';
+
+  export let timers: TMetrics['timers'];
+  export let timersUsages: TMetrics['timersUsages'];
+</script>
+
+{#each timers as api}
+  <div>
+    {#if api.invocations}
+      <strong>{api.name}:</strong>
+      <Number bind:value={api.invocations} />
+      {#if api.name === 'setTimeout'}
+        [<Number bind:value={timersUsages.timeouts.length} />]
+        {#each timersUsages.timeouts as v}
+          <li>
+            {v[0]}, [{#each v[1] as stack, index}
+              {#if index > 0}|{/if}<a href={stack.link}>{stack.name}</a>{/each}]
+          </li>
+        {/each}
+      {/if}
+      {#if api.name === 'setInterval'}
+        [<Number bind:value={timersUsages.intervals.length} />]
+        {#each timersUsages.intervals as v}
+          <li>
+            {v[0]}, [{#each v[1] as stack, index}
+              {#if index > 0}|{/if}<a href={stack.link}>{stack.name}</a>{/each}]
+          </li>
+        {/each}
+      {/if}
+    {/if}
+  </div>
+{/each}
