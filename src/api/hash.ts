bc4435556a113deb762f14e59c46025fe925a6f5
🕷️ <3756473+zendive@users.noreply.github.com>
2025-02-05 01:30:02 +0200

    evolve to progressive metrics patching

--- src/api/hash.ts
+++ src/api/hash.ts
@@ -4,9 +4,7 @@ import { bytesToHex } from '@noble/hashes/utils';
 const HASH_STRING_LENGTH = 64;
 
 export function hashString(str: string) {
-  if (str.length > HASH_STRING_LENGTH) {
-    return bytesToHex(sha256(str));
-  }
-
-  return str;
+  return (
+    (str.length - HASH_STRING_LENGTH > 0 && bytesToHex(sha256(str))) || str
+  );
 }
