e49a05bd7e0f8cdad5c599b8fe7a94423057b153
🕷️ <3756473+zendive@users.noreply.github.com>
2025-07-21 20:03:19 +0300

    detect extension code update

--- src/view/ConnectionAlert.svelte
+++ src/view/ConnectionAlert.svelte
@@ -2,31 +2,83 @@
   import { EMsg, portPost, runtimeListen } from '../api/communication.ts';
   import { Timer } from '../api/time.ts';
   import Alert from './shared/Alert.svelte';
-  import { INJECTION_ALERT_TIMEOUT } from './shared/const.ts';
+  import {
+    INJECTION_ALERT_TIMEOUT,
+    UPDATE_SENSOR_INTERVAL,
+  } from './shared/const.ts';
+  import { onMount } from 'svelte';
+  import { loadLocalStorage } from '../api/storage/storage.local.ts';
 
-  let alertEl: Alert | null = null;
+  let tabReloadAlertEl: Alert | null = null;
+  let devtoolsReloadAlertEl: Alert | null = null;
   const delayedAlert = new Timer(
     { delay: INJECTION_ALERT_TIMEOUT },
-    () => void alertEl?.show(),
+    () => void tabReloadAlertEl?.show(),
+  );
+  const extensionUpdateSensor = new Timer(
+    { delay: UPDATE_SENSOR_INTERVAL, repetitive: true },
+    () => {
+      whenUpdateDetected(() => {
+        devtoolsReloadAlertEl?.show();
+        extensionUpdateSensor.stop();
+      });
+    },
   );
 
   runtimeListen((o) => {
     if (o.msg === EMsg.INJECTION_CONFIRMED) {
       delayedAlert.stop();
-      alertEl?.hide();
+      tabReloadAlertEl?.hide();
     } else if (o.msg === EMsg.CONTENT_SCRIPT_LOADED) {
-      alertEl?.hide();
+      tabReloadAlertEl?.hide();
     }
   });
 
-  portPost({ msg: EMsg.CONFIRM_INJECTION });
-  delayedAlert.start();
+  onMount(() => {
+    pingContentScript();
+    extensionUpdateSensor.start();
+
+    return () => {
+      delayedAlert.stop();
+      extensionUpdateSensor.stop();
+    };
+  });
+
+  function pingContentScript() {
+    portPost({ msg: EMsg.CONFIRM_INJECTION });
+    delayedAlert.start();
+  }
+
+  /**
+   * Detect extension code update by waiting for an indirect
+   * symptom - when reading localStorage fails
+   * @NOTE: `chrome.runtime.onInstalled.addListener` doesn't work in
+   *    devtools panel script context
+   */
+  function whenUpdateDetected(callback: () => void) {
+    loadLocalStorage().catch((e: Error) => {
+      if (!e || e.message !== 'Extension context invalidated.') {
+        return;
+      }
+      try {
+        callback();
+      } catch (_) {}
+    });
+  }
 </script>
 
 <Alert
-  bind:this={alertEl}
+  bind:this={tabReloadAlertEl}
   dismissable={false}
   title="Attention"
 >
   <div>Tab reload required to continue live inspection</div>
+</Alert>
+
+<Alert
+  bind:this={devtoolsReloadAlertEl}
+  dismissable={false}
+  title="Attention"
+>
+  <div>Devtools reload required to continue live inspection</div>
 </Alert>
--- src/view/shared/const.ts
+++ src/view/shared/const.ts
@@ -3,6 +3,7 @@ export const AFTER_SCROLL_ANIMATION_CLASSNAME = 'scrolled-to';
 export const VARIABLE_ANIMATION_THROTTLE = 3500; // eye blinking average frequency
 export const FRAME_TIME_SAFE_THRESHOLD = 13.333333333333332; // ms
 export const INJECTION_ALERT_TIMEOUT = 1e3;
+export const UPDATE_SENSOR_INTERVAL = 2e3;
 export enum ESelfTimeDisplayMode {
   DEFAULT,
   STATS,
