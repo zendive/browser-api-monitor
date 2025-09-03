0fc7e8b6e476d975fda387175c6b40a9948c4dc9
🕷️ <3756473+zendive@users.noreply.github.com>
2025-09-03 04:24:03 +0300

    follow up `@noble/hashes` update
    Signed-off-by: 🕷️ <3756473+zendive@users.noreply.github.com>

--- src/api/hash.ts
+++ src/api/hash.ts
@@ -1,10 +1,11 @@
-import { sha256 } from '@noble/hashes/sha2';
-import { bytesToHex } from '@noble/hashes/utils';
+import { sha256 } from '@noble/hashes/sha2.js';
+import { bytesToHex, utf8ToBytes } from '@noble/hashes/utils.js';
 
 export const HASH_STRING_LENGTH = 64;
 
 export function hashString(str: string) {
   return (
-    (str.length - HASH_STRING_LENGTH > 0 && bytesToHex(sha256(str))) || str
+    (str.length - HASH_STRING_LENGTH > 0 &&
+      bytesToHex(sha256(utf8ToBytes(str)))) || str
   );
 }
