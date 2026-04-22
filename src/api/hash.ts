ebb8bf29b8d2328151d65d423912c915e0d4c247
🕷️ <3756473+zendive@users.noreply.github.com>
2026-04-22 19:54:14 +0300

    switch to `@awasm/noble` to apply "single-allocation" buffer
    Signed-off-by: 🕷️ <3756473+zendive@users.noreply.github.com>

--- src/api/hash.ts
+++ src/api/hash.ts
@@ -1,11 +1,13 @@
-import { sha256 } from '@noble/hashes/sha2.js';
+import { sha256 } from '@awasm/noble/js.js';
 
 const HASH_STRING_LENGTH = 64;
-const te = new TextEncoder();
+const te = /*@__PURE__*/ (() => new TextEncoder())();
+const uint8buffer = /*@__PURE__*/ (() => new Uint8Array(sha256.outputLen))();
 
 export function hashString(str: string) {
   return (
-    (str.length - HASH_STRING_LENGTH > 0 && sha256(te.encode(str)).toHex()) ||
+    (str.length - HASH_STRING_LENGTH > 0 &&
+      sha256(te.encode(str), { out: uint8buffer }).toHex()) ||
     str
   );
 }
